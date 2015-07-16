c8a2b95c4e9a2c775dc77e773bb9497384e7d67f
Alejandro <Alejandro@172.20.8.192>
2015-07-15 20:09:17 -0600

    Agregando comentarios

--- Radiojava/src/InterfazRadio.java
+++ Radiojava/src/InterfazRadio.java
@@ -1,5 +1,7 @@
-
+/*Esta clase es la interfaz de la Radio*/	
 public interface InterfazRadio {
+	
+	/*Se declaran cada uno de los Atributos con sus caracteristicas*/	
 	final int CONST_CAMBIO_AM = 10;
 	final double CONST_CAMBIO_FM = 0.2; 
 	final int MAX_AM = 1610;
@@ -7,7 +9,7 @@ public interface InterfazRadio {
 	final double MAX_FM = 107.9;
 	final double MIN_FM = 87.9;
 	
-
+	/*Se declaran cada uno de los metodos a Utilizar de la Radio*/	
 	public void encenderApagar();
 	
 	public void cambiarFrec();
