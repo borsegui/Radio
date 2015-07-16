9e19f5b2c506fb992e6aae49c1633f9598f0f745
Alejandro <Alejandro@172.20.8.192>
2015-07-15 19:34:24 -0600

    Se declararo atributos de esta clase

--- Radiojava/src/InterfazRadio.java
+++ Radiojava/src/InterfazRadio.java
@@ -1,4 +1,9 @@
 
-public class InterfazRadio {
-
+public interface InterfazRadio {
+	final int CONST_CAMBIO_AM = 10;
+	final double CONST_CAMBIO_FM = 0.2; 
+	final int MAX_AM = 1610;
+	final int MIN_AM = 530;
+	final double MAX_FM = 107.9;
+	final double MIN_FM = 87.9;
 }
