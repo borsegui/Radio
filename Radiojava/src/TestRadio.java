bfa757f86650142582f9de9c5c63f1154ce22696
Alejandro <Alejandro@172.20.8.192>
2015-07-15 19:58:34 -0600

    TestRadio JUnit

--- Radiojava/src/TestRadio.java
+++ Radiojava/src/TestRadio.java
@@ -1,4 +1,25 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 
 public class TestRadio {
+	
+	Radio test = new Radio();
+	@Test
+	//Para sabe si se esta dando el cambio de estado de encendido a apagado
+	public void testEstado() {
+		boolean encendido = true;
+		test.encenderApagar();
+		assertEquals(encendido, test.getEstado());
+	}
+	
+	@Test
+	//Para saber si se esta subiendo el volumen del radio
+	public void testVolumen() {
+		int esperado = 1;
+		test.subirVolumen();
+		assertEquals(esperado, test.getVolumen());
+	}
 
 }
