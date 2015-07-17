5494e8d4cc3969ac2a0ff4064b1af43eeec7bdcb
Alejandro <Alejandro@172.20.8.192>
2015-07-16 19:31:13 -0600

    Radio final comentado

--- Radiojava/src/Radio.java
+++ Radiojava/src/Radio.java
@@ -1,124 +1,167 @@
-/* UNIVERSIDAD DEL VALLE DE GUATEMALA
-  CHISTOPHER CHIROY
-  BORIS CIFUENTES
-  DIEGO DE LEÓN 
-  
-Esta clase  a cada uno de los metodos se crean los sets ylos gets 
+/**
+ * UNIVERSIDAD DEL VALLE DE GUATEMALA
+ * @author   CHISTOPHER CHIROY, BORIS CIFUENTES,DIEGO DE LEÓN 
+ *
+ * Esta clase  a cada uno de los metodos se crean los sets ylos gets 
 para poderlos utilizar en otra clases , también crean constructores cony sin
 atributos
-*/	
+ */
 
-/* se declaran las variables, listas con sus caracteristicas*/
+
+/**
+ * @author
+ *se declaran las variables, listas con sus caracteristicas
+ */
+
 public class Radio implements InterfazRadio{
-	private double estacion;
+	private double emisora;
 	private int volumen;
-	private boolean estado;
-	private boolean frecuencia;
+	private boolean encendido;
+	private String frecuencia;
 	private double[][] favoritos;
 	
 	public Radio(){
 		favoritos = new double[2][12];
-		estacion = MIN_AM;
+		emisora = MIN_AM;
 		volumen = 0;
-		estado = false;
-		frecuencia = false;
+		encendido = false;
+		frecuencia = "AM";
 	}
 	
-
-	/*se declaran los gets*/
+	/**
+	 * @author 
+	 * se decalran sets y gets
+	 */
 	
-	public double getEstacion() {
-		return estacion;
-	}
+	
+	/* (non-Javadoc)
+	 * @see InterfazRadio#getVolumen()
+	 */
 	public int getVolumen() {
 		return volumen;
 	}
-	public boolean getEstado() {
-		return estado;
-	}
-	public boolean getFrecuencia() {
-		return frecuencia;
-	}
+	/**
+	 * @return
+	 */
 	public double[][] getFavoritos(){
 		return favoritos;
 	}
-
-	/*se declaran los sets*/
-	public void setEstacion(double estacion) {
-		this.estacion = estacion;
+	/* (non-Javadoc)
+	 * @see InterfazRadio#getEmisora()
+	 */
+	public double getEmisora() {
+		return emisora;
 	}
-	public void setVolumen(int volumen) {
-		this.volumen = volumen;
+	/* (non-Javadoc)
+	 * @see InterfazRadio#getEncendido()
+	 */
+	public boolean getEncendido() {
+		return encendido;
 	}
-	public void setEstado(boolean estado) {
-		this.estado = estado;
-	}
-	public void setFrecuencia(boolean frecuencia) {
-		this.frecuencia = frecuencia;
-	}
+	/* (non-Javadoc)
+	 * @see InterfazRadio#getFrec()
+	 */
+	public String getFrec(){
+		// TODO Auto-generated method stub
+		return frecuencia;
+	} 
+	
+	/**
+	 * @param favoritos se crea favoritos con sus parametros
+	 */
 	public void setFavoritos(double[][] favoritos){
 		this.favoritos = favoritos;
 	}
 
 
+	/* (non-Javadoc)
+	 * @see InterfazRadio#encenderApagar()
+	 */
 	@Override
 	public void encenderApagar() {
-		if (estado==false)
-			estado=true;
+		if (encendido==false)
+			encendido=true;
 		else
-			estado = false;
+			encendido = false;
 	}
+	/* (non-Javadoc)
+	 * @see InterfazRadio#cambiarFrec()
+	 */
 	@Override
 	public void cambiarFrec() {
-		if (frecuencia==false){
-			frecuencia=true;
-			estacion=MIN_FM;
+		if (frecuencia.equals("AM")){
+			frecuencia="FM";
+			emisora=MIN_FM;
 		}
 		else{
-			frecuencia = false;
-			estacion=MIN_AM; 
+			frecuencia ="AM";
+			emisora=MIN_AM; 
 		}
 	}
+	/* (non-Javadoc)
+	 * @see InterfazRadio#cargarEmisora(int)
+	 * estacion=emisora;
+	 */
 	@Override
-	public void cargarEmisora(double emisora) {
-		estacion=emisora;
+	public void cargarEmisora(int posicion) {
+		double est=0;
+		if (frecuencia.equals("FM"))
+			est=favoritos[0][posicion-1];
+		else{
+			est=favoritos[1][posicion-1];
+		}
+		if (est!=0){
+			emisora=est;
+		}
 		
 	}
+	/* (non-Javadoc)
+	 * @see InterfazRadio#adelantarEmisora()
+	 */
 	@Override
 	public void adelantarEmisora() {
-		if (frecuencia==true){
-			estacion=estacion+CONST_CAMBIO_FM;
-			if(estacion>MAX_FM)
-				estacion=MIN_FM;
+		if (frecuencia.equals("FM")){
+			emisora=emisora+CONST_CAMBIO_FM;
+			if(emisora>MAX_FM)
+				emisora=MIN_FM;
 		}
 		else{
-			estacion=estacion+CONST_CAMBIO_AM;
-			if(estacion>MAX_AM)
-				estacion=MIN_AM;
+			emisora=emisora+CONST_CAMBIO_AM;
+			if(emisora>MAX_AM)
+				emisora=MIN_AM;
 		}
 	}
+	/* (non-Javadoc)
+	 * @see InterfazRadio#atrasarEmisora()
+	 */
 	@Override
 	public void atrasarEmisora() {
-		if (frecuencia==true){
-			estacion=estacion-CONST_CAMBIO_FM;
-			if(estacion<MIN_FM)
-				estacion=MAX_FM;
+		if (frecuencia=="FM"){
+			emisora=emisora-CONST_CAMBIO_FM;
+			if(emisora<MIN_FM)
+				emisora=MAX_FM;
 		}
 		else{
-			estacion=estacion-CONST_CAMBIO_AM;
-			if(estacion<MIN_AM)
-				estacion=MAX_AM;
+			emisora=emisora-CONST_CAMBIO_AM;
+			if(emisora<MIN_AM)
+				emisora=MAX_AM;
 		}
 	}
+	/* (non-Javadoc)
+	 * @see InterfazRadio#guardarEmisora(double, int)
+	 */
 	@Override
 	public void guardarEmisora(double emisora, int posicion) {
-		if (frecuencia==true)
-			favoritos[0][posicion-1]=estacion;
+		if (frecuencia.equals("FM"))
+			favoritos[0][posicion-1]=this.emisora;
 		else{
-			favoritos[1][posicion-1]=estacion;
+			favoritos[1][posicion-1]=this.emisora;
 		}
 		
 	}
+	/* (non-Javadoc)
+	 * @see InterfazRadio#subirVolumen()
+	 */
 	@Override
 	public void subirVolumen() {
 		// TODO Auto-generated method stub
@@ -127,11 +170,14 @@ public class Radio implements InterfazRadio{
 			volumen=15;
 	}
 	
+	/* (non-Javadoc)
+	 * @see InterfazRadio#bajarVolumen()
+	 */
 	@Override
 	public void bajarVolumen() {
 		// TODO Auto-generated method stub
 		volumen = volumen-1;
 		if(volumen<0)
 			volumen=0;
-	} 
+	}
 }
